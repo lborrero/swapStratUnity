597728317f0eb54d142eb51db840d72e7a96faea
Leonardo Borrero Luz <leonardo_borrero_luz@live.com>
2014-04-10 21:56:45 -0400

    initialized unity project

---
+++ swapStratUnity/Assets/SwapBoard.cs
@@ -0,0 +1,35 @@
+﻿	using UnityEngine;
+using System.Collections;
+
+public class SwapBoard : MonoBehaviour {
+
+	public GameObject swapTilePrefab;
+
+	private int _width = 6;
+	private int _height = 6;
+	private float _xOffset;
+	private float _yOffset;
+
+	// Use this for initialization
+	void Start () {
+		_xOffset = _width / -2 + 0.5f;
+		_yOffset = _height / -2 + 0.5f;
+
+		int idCounter = 1;
+		for(int i=0; i<_height; i++)
+		{
+			for(int j=0; j<_width; j++)
+			{
+				GameObject tmp = (GameObject)Instantiate(swapTilePrefab.gameObject, new Vector3(_xOffset + i,0,_yOffset + j) , transform.rotation);
+				tmp.GetComponent<Tile>().SetCoordinates(i, j);
+				tmp.GetComponent<Tile>().SetTileId(idCounter);
+				idCounter++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ swapStratUnity/Assets/Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public GameObject physicalTile;
+	private int _tileId;
+	private int _xPos;
+	private int _yPos;
+
+	public void SetTileId(int tildeId)
+	{
+		_tileId = tildeId;
+	}
+
+	public void SetCoordinates(int x, int y)
+	{
+		_xPos = x;
+		_yPos = y;
+	}
+
+	void OnMouseDown()
+	{
+		print ("Tile OnMouseDown: tileId: " + _tileId + " coordinates: " + _xPos + "," + _yPos);
+	}
+}
