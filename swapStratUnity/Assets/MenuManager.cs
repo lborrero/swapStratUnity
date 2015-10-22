dad4e6c9ba57402d695deea3c503a2512319c935
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2015-10-22 08:23:19 -0400

    integrating ads

--- swapStratUnity/Assets/MenuManager.cs
+++ swapStratUnity/Assets/MenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using GoogleMobileAds.Api;
 
 public class MenuManager : MonoBehaviour {
 
@@ -10,12 +11,14 @@ public class MenuManager : MonoBehaviour {
 	public GameObject EndScreen;
 	public sGameManager.GeneralGameState defaultGameState;
 	public Text winnerLabel;
+	BannerView bannerView;
 
 	// Use this for initialization
 	void Start () {
 		sgm = sGameManager.Instance;
 		sbm = sBoardManager.Instance;
 		UpdateGameState ((int)defaultGameState);
+		RequestBanner ();
 	}
 
 	public void UpdateGameState(int gameState)
@@ -26,13 +29,16 @@ public class MenuManager : MonoBehaviour {
 		case sGameManager.GeneralGameState.startScreen:
 			StartScreen.SetActive(true);
 			EndScreen.SetActive(false);
+			bannerView.Show();
 			break;
 		case sGameManager.GeneralGameState.gameMode:
 			StartScreen.SetActive(false);
 			EndScreen.SetActive(false);
+			bannerView.Hide();
 			break;
 		case sGameManager.GeneralGameState.endScreen:
 			StartScreen.SetActive(false);
+			bannerView.Show();
 			if(sbm.player1.currentTurnPointCount > sbm.player2.currentTurnPointCount)
 			{
 				winnerLabel.text = "Blue Wins!";
@@ -48,5 +54,26 @@ public class MenuManager : MonoBehaviour {
 			EndScreen.SetActive(true);
 			break;
 		}
+	}
+
+	private void RequestBanner()
+	{
+		#if UNITY_ANDROID
+		string adUnitId = "INSERT_ANDROID_BANNER_AD_UNIT_ID_HERE";
+		#elif UNITY_IPHONE
+		string adUnitId = "INSERT_IOS_BANNER_AD_UNIT_ID_HERE";
+		#else
+		string adUnitId = "unexpected_platform";
+		#endif
+		
+		// Create a 320x50 banner at the top of the screen.
+		bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder()
+			.AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+			.AddTestDevice("0BC20B01631B96FBFB903107F1CFE7B4")  // My test device.
+			.Build();
+		// Load the banner with the request.
+		bannerView.LoadAd(request);
 	}
 }
