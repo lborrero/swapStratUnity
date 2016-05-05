c5818c05c7993f0a753c8aa2fe2e0079a994a254
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2016-05-05 14:45:38 -0400

    added new stuff for game setup VO

---
+++ swapStratUnity/Assets/Scripts/GameSetupEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameSetupVO))]
+public class GameSetupEditor : Editor {
+
+	enum tileType
+	{
+		red = 0,
+		blue,
+		empty,
+		nothing
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		GameSetupVO myGSVO = (GameSetupVO) target;
+
+//		EditorGUI.IntField (Rect(0,0, 100,100), "Experience", myGSVO.random);
+
+//		EditorGUILayout.BeginHorizontal("Box", GUILayout.ExpandWidth(false));
+//
+//		EditorGUILayout.BeginVertical ("square", GUILayout.ExpandWidth(false));
+//
+//		EditorGUILayout.IntField (Rect(0,35,position.width,15),"1", myGSVO.random, GUILayout.ExpandWidth(false));
+//		EditorGUILayout.IntField ("1", myGSVO.random, GUILayout.ExpandWidth(false));
+//		EditorGUILayout.EndVertical ();
+//
+//		EditorGUILayout.IntField ("1", myGSVO.random, GUILayout.ExpandWidth(false));
+//		EditorGUILayout.IntField ("1", myGSVO.random, GUILayout.ExpandWidth(false));
+//		EditorGUILayout.EndHorizontal();
+
+		if(GUILayout.Button("BuildObject"))
+		{
+			myGSVO.someFunction ();
+		}
+	}
+
+
+}
---
+++ swapStratUnity/Assets/Scripts/GameSetupVO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetupVO : MonoBehaviour {
+
+	public int random;
+	public int[] board;
+
+	public void someFunction()
+	{
+		
+	}
+}
