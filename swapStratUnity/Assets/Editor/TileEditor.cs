fb3c2ff56350da929b0a007075c8264656d1e204
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2017-10-18 14:53:54 -0700

    Adding editors to tiles and tokens

---
+++ swapStratUnity/Assets/Editor/TileEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Tile))]
+public class TileEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		Tile myscript = (Tile)target;
+		if (GUILayout.Button ("Update Tile"))
+		{
+			myscript.UpdateState ();
+		}
+	}
+}
---
+++ swapStratUnity/Assets/Editor/TokenEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Token))]
+public class TokenEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		Token myscript = (Token)target;
+		if (GUILayout.Button ("Update Token"))
+		{
+			myscript.UpdateState ();
+		}
+	}
+}
